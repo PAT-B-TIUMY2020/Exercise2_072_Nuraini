2f59846f3c2103ea15e581a03595c8f5f666e5ec
Nuraini <nurroplet45@gmail.com>
2020-12-25 23:11:56 +0700

    Selesai

---
+++ Client_072_Nuraini/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace Client_072_Nuraini
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tNIM = new System.Windows.Forms.TextBox();
+            this.tNama = new System.Windows.Forms.TextBox();
+            this.tProdi = new System.Windows.Forms.TextBox();
+            this.tAngkatan = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NIM";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nama";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Prodi";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(40, 192);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Angkatan";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Total Data :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(117, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 5;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(197, 42);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 13);
+            this.label7.TabIndex = 6;
+            // 
+            // tNIM
+            // 
+            this.tNIM.Location = new System.Drawing.Point(120, 81);
+            this.tNIM.Name = "tNIM";
+            this.tNIM.Size = new System.Drawing.Size(329, 20);
+            this.tNIM.TabIndex = 7;
+            // 
+            // tNama
+            // 
+            this.tNama.Location = new System.Drawing.Point(120, 116);
+            this.tNama.Name = "tNama";
+            this.tNama.Size = new System.Drawing.Size(329, 20);
+            this.tNama.TabIndex = 8;
+            // 
+            // tProdi
+            // 
+            this.tProdi.Location = new System.Drawing.Point(120, 151);
+            this.tProdi.Name = "tProdi";
+            this.tProdi.Size = new System.Drawing.Size(329, 20);
+            this.tProdi.TabIndex = 9;
+            // 
+            // tAngkatan
+            // 
+            this.tAngkatan.Location = new System.Drawing.Point(120, 189);
+            this.tAngkatan.Name = "tAngkatan";
+            this.tAngkatan.Size = new System.Drawing.Size(329, 20);
+            this.tAngkatan.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(497, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Insert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(497, 73);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Find All";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(497, 118);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Search";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(497, 163);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "Update";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(497, 206);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 15;
+            this.button5.Text = "Delete";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(497, 249);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 16;
+            this.button6.Text = "Sum";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(497, 294);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 17;
+            this.button7.Text = "Clear";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(43, 249);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(406, 152);
+            this.dataGridView1.TabIndex = 18;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(610, 442);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tAngkatan);
+            this.Controls.Add(this.tProdi);
+            this.Controls.Add(this.tNama);
+            this.Controls.Add(this.tNIM);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tNIM;
+        private System.Windows.Forms.TextBox tNama;
+        private System.Windows.Forms.TextBox tProdi;
+        private System.Windows.Forms.TextBox tAngkatan;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Client_072_Nuraini/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ServiceModel;
+using Client_072_Nuraini;
+
+namespace Client_072_Nuraini
+{
+
+    public partial class Form1 : Form
+
+    {
+        ClassData classData = new ClassData();
+        ServiceHost hostObject;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (tNIM.Text != "" &&
+               tNama.Text != "" &&
+               tProdi.Text != "" &&
+               tAngkatan.Text != "")
+            {
+                if (tNIM.Text.Length <= 12 &&
+                tAngkatan.Text.Length <= 4 &&
+                tProdi.Text.Length <= 30 &&
+                tNama.Text.Length <= 20)
+                {
+                    try
+                    {
+                        string nim = tNIM.Text;
+                        string nama = tNama.Text;
+                        string prodi = tProdi.Text;
+                        string angkatan = tAngkatan.Text;
+                        classData.insertMahasiswa(nim, nama, prodi, angkatan);
+                        clear();
+                        dataGridView1.DataSource = classData.getAllData();
+                        MessageBox.Show("Data successfuly inserted");
+                    }
+                    catch (Exception ex)
+                    {
+                        label7.Text = "Server Error";
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please check your data");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please check your data");
+            }
+
+        }
+
+        public void clear()
+        {
+            tNIM.Text = "";
+            tNama.Text = "";
+            tProdi.Text = "";
+            tAngkatan.Text = "";
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button2.Enabled = true;
+            label7.Text = "";
+            dataGridView1.DataSource = null;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                dataGridView1.DataSource = classData.getAllData();
+            }
+            catch
+            {
+                label7.Text = "Server error";
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            tNIM.Text = Convert.ToString(dataGridView1.Rows[e.RowIndex].Cells[1].Value);
+            tNama.Text = Convert.ToString(dataGridView1.Rows[e.RowIndex].Cells[0].Value);
+            tProdi.Text = Convert.ToString(dataGridView1.Rows[e.RowIndex].Cells[2].Value);
+            tAngkatan.Text = Convert.ToString(dataGridView1.Rows[e.RowIndex].Cells[3].Value);
+
+            button4.Enabled = true;
+            button3.Enabled = true;
+            button2.Enabled = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (tNIM.Text != "")
+            {
+                string nim = tNIM.Text;
+                List<Mahasiswa> mhs = new List<Mahasiswa>();
+                mhs.Add(classData.search(nim));
+                clear();
+                dataGridView1.DataSource = mhs;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (tNIM.Text != "" &&
+               tNama.Text != "" &&
+               tProdi.Text != "" &&
+               tAngkatan.Text != "")
+            {
+                if (tNIM.Text.Length <= 12 &&
+                tAngkatan.Text.Length <= 4 &&
+                tProdi.Text.Length <= 30 &&
+                tNama.Text.Length <= 20)
+                {
+                    try
+                    {
+                        Mahasiswa mhs = new Mahasiswa();
+                        mhs.nim = tNIM.Text;
+                        mhs.nama = tNama.Text;
+                        mhs.prodi = tProdi.Text;
+                        mhs.angkatan = tAngkatan.Text;
+
+                        ClassData classData = new ClassData();
+                        classData.updateDatabase(mhs);
+                        MessageBox.Show("Data successfuly updated");
+                        clear();
+                        dataGridView1.DataSource = classData.getAllData();
+                    }
+                    catch
+                    {
+                        label7.Text = "Server Error";
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please check your data");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please check your data");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you want to delete", "Delete", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                try
+                {
+                    ClassData classData = new ClassData();
+                    classData.deleteMahasiswa(tNIM.Text);
+                    clear();
+                    dataGridView1.DataSource = classData.getAllData();
+                    MessageBox.Show("Data successfuly deleted");
+                }
+                catch (Exception ex)
+                {
+                    label7.Text = "Server Error";
+                }
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string data = classData.sumData();
+                label5.Enabled = true;
+                label6.Text = data.ToString();
+            }
+            catch (Exception ex)
+            {
+                label7.Text = "Server Error";
+            }
+        }
+    }
+}
