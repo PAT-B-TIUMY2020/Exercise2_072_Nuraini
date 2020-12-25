3860010f50495cc182d76d3a699fa1b41abb66f8
Nuraini <nurroplet45@gmail.com>
2020-12-25 22:06:03 +0700

    Add project files.

---
+++ Server_072_Nuraini/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Service_072_Nuraini;
+using System.ServiceModel;
+using System.Windows.Forms;
+
+namespace Server_072_Nuraini
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
